6a3b2108a357dba891fd9fa542298e923488ddf8
Peter Melchior <peter.m.melchior@gmail.com>
2022-06-23 18:12:42 -0400

    shroom as class

--- README.md
+++ README.md
@@ -1,25 +1,36 @@
-# Mismatch Detection in Tabular Data
+# Discrepancy Detection in Tabular Data
 
 Several tests exist to quantify how well two data distributions match.
-But if they don't match well, it's difficult in high-dimensional spaces to find prototypical examples of discrepant samples.
-Our method performs a dimensionality reduction by a Self-Organzing Map, finds regions of significant differences in the maps for both data sets, and aggregates samples from those regions as examples of over/under-represented features between the data sets.
+But if they don't match well, it's difficult to find prototypical examples of discrepancies, especially in high dimensions.
+Our method performs a dimensionality reduction by a Self-Organzing Map, finds regions of significant differences in the maps for both data sets, and selects or aggregates samples from those regions as examples of over/under-represented features between the data sets.
 
 The setup is simple:
 
 ```python
 
-from shroom import get_som, analyze
+from shroom import Shroom
 
 # load two samples (train and test) as numpy arrays
 # ...
 
-# get SOM based on the training data
-# the default uses best-guess parameters of the SOM
-# optimization is possible but slow
-som = get_som(train, optimize=False)
+# make SOM based on the training data, apply to test data
+# the default SOM uses best-guess parameters; optimization is possible but slow
+m = Shroom(train, test)
 
-# make frequency maps for train and test as well as their difference
-# connect regions with significant differences and aggregate samples therein
-# set plot=True for visualization (labels correspond to names of fields in train/test)
-aggregates_above, aggregates_below = analyze(som, train, test, plot=True, labels=labels)
+# If you already have a SOM you want to work with
+m = Shroom(train, test, som=som)
+
+# find all groups of SOM cells with higher or lower density in test than in train
+# significance and minimum cell number per group are configurable
+groups_above, groups_below = m.find_discrepancies()
+
+# show a over/under-dense group in data space
+m.show(groups_above[0])
+
+# combine all samples in `test` that fall in the cells of `group`
+# optional: show the position of aggregate in data space
+m.aggregate(groups_above[0], test, plot=True, labels=labels)
+
+# select `N` examples from `test` that fall in the cells of `group`
+m.pick(groups_above[0], test, N=10, method="sample")
 ```
--- shroom.py
+++ shroom.py
@@ -8,7 +8,6 @@ objective = lambda data: data.std() / data.mean()
 
 def optimize_som(train, bounds):
 
-
     # minimize std of SOM activation
     def std_sigma(params):
         L1, L2, sigma, lr = params
@@ -48,87 +47,6 @@ def get_som(data, optimize=False):
     return som
 
 
-# bootstrap resampling
-def get_diff_dispersion(som, data, R=10):
-    N = len(data)
-    freq = som.activation_response(data)
-    diffs = []
-    for _ in range(R):
-        bootstrap_idx = np.random.choice(N, size=N, replace=True)
-        data_ = data[bootstrap_idx]
-        freq_ = som.activation_response(data_)
-        diff_ = (freq_ - freq)/N
-        diffs.append(diff_)
-    diff_std = np.array(diffs).std()
-    return diff_std
-
-def get_maps(som, train, test, sig=2):
-
-    # get direct projections
-    freq = som.activation_response(train)
-    freq_test = som.activation_response(test)
-    print (f"SOM objective: { objective(freq) }")
-
-    # difference map: difference of normalized distributions
-    diff = freq_test / freq_test.sum() -  freq / freq.sum()
-
-    # find cells significantly below or above the mean in the difference map
-    train_std = get_diff_dispersion(som, train)
-    test_std = get_diff_dispersion(som, test)
-    above = diff > diff.mean() + sig*(train_std + test_std)
-    below = diff < diff.mean() - sig*(train_std + test_std)
-
-    return freq, freq_test, diff, above, below
-
-
-def connect_cells(detection, minimum=5, structure=None):
-    labeled_im, num_features = scipy.ndimage.label(detection, structure=structure)
-    idx = np.indices(detection.shape)
-    groups = []
-    for i in range(num_features):
-        cells = labeled_im == i+1
-        cell_idxs = np.array((idx[0][cells], idx[1][cells])).T
-        groups.append(cell_idxs)
-
-    # order by largest group
-    sizes = tuple(len(cells) for cells in groups)
-    order = np.argsort(sizes)
-    groups = tuple(groups[i] for i in order[::-1] if sizes[i] >= minimum)
-
-    # update labeled image
-    labeled_im = np.zeros(detection.shape, dtype=int)
-    for i, group in enumerate(groups):
-        for cell in group:
-            labeled_im[tuple(cell)] = i+1
-
-    return groups, labeled_im
-
-def aggregate(som, data, cells, weight=None, win_map=None):
-
-    if win_map is None:
-        # get all cell ids for data
-        win_map = som.win_map(data, return_indices=True)
-
-    # for every group in below:
-    # aggregate all samples in train that belong to this group
-    samples = np.zeros(data.shape[1])
-    total = 0
-    for cell in cells:
-        cell_samples = data[win_map[tuple(cell)]]
-        if weight is None:
-            w = 1
-        else:
-            w = weight[tuple(cell)]
-        samples += cell_samples.sum(axis=0) * w
-        total += len(cell_samples) * w
-        sample_mean = samples / total
-    return sample_mean, total
-
-def aggregate_groups(som, data, groups, weight=None):
-    # get all cell ids for data
-    win_map = som.win_map(data, return_indices=True)
-    return tuple(aggregate(som, data, group, weight=weight, win_map=win_map) for group in groups)
-
 def plot_maps(freq, freq_test, diff, labeled_above, labeled_below):
     fig, axes = plt.subplots(1, 5, figsize=(12,4))
     axes[0].imshow(freq, cmap='Greys')
@@ -145,35 +63,195 @@ def plot_maps(freq, freq_test, diff, labeled_above, labeled_below):
     axes[4].set_title("Groups Below");
     return fig
 
+from matplotlib.lines import Line2D
 
-def plot_data(train, test, **kwargs):
-    fig = corner.corner(train, hist_kwargs={"density": True});
-    corner.corner(test, fig=fig, color='C1', hist_kwargs={"density": True}, **kwargs);
+def plot_data(train, test, subset=None, subset_label='Subset', **kwargs):
+    fig = corner.corner(train, **kwargs);
+
+    # get bins of 1d histograms:
+    # bit of a hack, but we want to make these histograms as comparable as possible
+    D = train.shape[1]
+    bins = []
+    legend_bits = []
+    for d in range(D):
+        panel = d*D + d # diagonal panels in corner plot
+        ax = fig.axes[panel]
+        hist = ax.get_children()[0]
+        bins_ = np.unique(hist.get_xy()[:,0])
+        bins.append(bins_)
+        lw = hist.get_lw()
+        color = hist.get_ec()
+        if d == 0:
+            legend_bits += [(Line2D([0], [0], color=color, lw=lw), 'Train')]
+
+    # instead of regular histogram, need to go through 1D smoothing to provide exact bin edges:
+    # see https://github.com/dfm/corner.py/issues/173
+    # match the sample sizes via weights
+    weights = len(train) / len(test) * np.ones(len(test))
+    corner.corner(test, bins=bins, smooth1d=1e-6, weights=weights, fig=fig, color='C1', hist_kwargs={"lw": lw});
+    legend_bits += [(Line2D([0], [0], color='C1', lw=lw), 'Test')]
+
+    if subset is not None:
+        # show relative abundance of subset vs test
+        weights = len(subset) / len(test) * len(train) / len(test) * np.ones(len(subset))
+        corner.corner(subset, bins=bins, smooth1d=1e-6, weights=weights, fig=fig, color='C3', hist_kwargs={"lw": lw});
+        legend_bits += [(Line2D([0], [0], color='C3', lw=lw), subset_label)]
+
+    # add legend to first panel
+    ax = fig.axes[0]
+    lines, names = zip(*legend_bits)
+    ax.legend(lines, names, frameon=False)
+
     return fig
 
-def analyze(som, train, test, sig=3, min_cells=5, plot=True, labels=None):
-    # make frequency maps and their differences
-    freq, freq_test, diff, above, below = get_maps(som, train, test, sig=sig)
-    # find connected groups: overdensities often ridges, so use broader
-    # structure kernel (connected to all 8 neighbor cells)
-    groups_above, labeled_above = connect_cells(above, minimum=min_cells, structure=np.ones((3,3)))
-    groups_below, labeled_below = connect_cells(below, minimum=min_cells)
-    print (f"Detected {len(groups_above)} groups above threshold")
-    print (f"Detected {len(groups_below)} groups below threshold")
 
-    if plot:
-        plot_maps(freq, freq_test, diff, labeled_above, labeled_below)
 
-    # aggregate train where it is more abundant than test: what's missing from test
-    aggregates_below = aggregate_groups(som, train, groups_below, weight=-diff)
+class Shroom():
 
-    # aggregate in reverse: where test is overdense compared to training
-    aggregates_above = aggregate_groups(som, test, groups_above, weight=diff)
+    def __init__(self, train, test, som=None, verbose=True):
+        self._train = train
+        self._test = test
+        if som is None:
+            som = get_som(self._train)
+        self._som = som
+        self.verbose = verbose
+        self._make_maps()
 
-    if plot:
-        for mean, sig in aggregates_above:
-            plot_data(train, test, labels=labels, truths=mean, truth_color='C3')
-        for mean, sig in aggregates_below:
-            plot_data(train, test, labels=labels, truths=mean, truth_color='C0')
+    def _make_maps(self):
+        # get direct projections
+        self.map_train = self._som.activation_response(self._train)
+        self.map_test = self._som.activation_response(self._test)
 
-    return aggregates_above, aggregates_below
+        if self.verbose:
+            print (f"SOM objective: { objective(self.map_train) } (lower is better)")
+
+        # difference map: difference of normalized distributions
+        self.map_diff = self.map_test / self.map_test.sum() -  self.map_train / self.map_train.sum()
+
+        # win map: sample index -> map cell
+        self._win_map = {}
+        self._win_map["train"] = self._som.win_map(self._train, return_indices=True)
+        self._win_map["test"] = self._som.win_map(self._test, return_indices=True)
+
+    def find_discrepancies(self, sig=3, min_cells=5, plot=True):
+
+        # find cells significantly below or above the mean in the difference map
+        if not hasattr(self, "_train_std"):
+            self._train_std = self._get_diff_dispersion(self._som, self._train)
+        if not hasattr(self, "_test_std"):
+            self._test_std = self._get_diff_dispersion(self._som, self._test)
+
+        threshold = sig*(self._train_std + self._test_std)
+        map_above = self.map_diff > self.map_diff.mean() + threshold
+        map_below = self.map_diff < self.map_diff.mean() - threshold
+
+        # find connected groups: overdensities often ridges, so use broader
+        # structure kernel (connected to all 8 neighbor cells)
+        groups_above, labeled_above = self._connect_cells(map_above, minimum=min_cells, structure=np.ones((3,3)))
+        groups_below, labeled_below = self._connect_cells(map_below, minimum=min_cells)
+
+        if self.verbose:
+            print (f"Detected {len(groups_above)} groups above threshold")
+            print (f"Detected {len(groups_below)} groups below threshold")
+
+        if plot:
+            plot_maps(self.map_train, self.map_test, self.map_diff, labeled_above, labeled_below)
+
+        return groups_above, groups_below
+
+    def show(self, group, label="Group", **kwargs):
+        data = self._train
+        win_map = self._som.win_map(data, return_indices=True)
+        samples = np.concatenate(tuple(data[win_map[tuple(cell)]] for cell in group), axis=0)
+        return plot_data(self._train, self._test, subset=samples, subset_label=label, **kwargs)
+
+    # bootstrap resampling
+    def _get_diff_dispersion(self, som, data, R=10):
+        N = len(data)
+        freq = som.activation_response(data)
+        diffs = []
+        for _ in range(R):
+            bootstrap_idx = np.random.choice(N, size=N, replace=True)
+            data_ = data[bootstrap_idx]
+            freq_ = som.activation_response(data_)
+            diff_ = (freq_ - freq)/N
+            diffs.append(diff_)
+        diff_std = np.array(diffs).std()
+        return diff_std
+
+    def _connect_cells(self, detection, minimum=5, structure=None):
+        labeled_im, num_features = scipy.ndimage.label(detection, structure=structure)
+        idx = np.indices(detection.shape)
+        groups = []
+        for i in range(num_features):
+            cells = labeled_im == i+1
+            cell_idxs = np.array((idx[0][cells], idx[1][cells])).T
+            groups.append(cell_idxs)
+
+        # order by largest group
+        sizes = tuple(len(cells) for cells in groups)
+        order = np.argsort(sizes)
+        groups = tuple(groups[i] for i in order[::-1] if sizes[i] >= minimum)
+
+        # update labeled image
+        labeled_im = np.zeros(detection.shape, dtype=int)
+        for i, group in enumerate(groups):
+            for cell in group:
+                labeled_im[tuple(cell)] = i+1
+
+        return groups, labeled_im
+
+    def aggregate(self, group, data, weight=None, plot=False, **kwargs):
+        # TODO: win map should be cached if it's only for train and test
+        win_map = self._som.win_map(data, return_indices=True)
+        samples = np.zeros(data.shape[1])
+        total = 0
+        for cell in group:
+            cell_samples = data[win_map[tuple(cell)]]
+            if weight is None:
+                w = 1
+            else:
+                w = weight[tuple(cell)]
+            samples += cell_samples.sum(axis=0) * w
+            total += len(cell_samples) * w
+            sample_mean = samples / total
+
+        if plot:
+            plot_data(self._train, self._test, truths=sample_mean, **kwargs)
+
+        return sample_mean, total
+
+    def pick(self, group, data, N=1, method="max", plot=False, **kwargs):
+        assert method in ["max", "sample"]
+
+        win_map = self._som.win_map(data, return_indices=True)
+
+        if method == "max":
+            # select cell with largest discrepancy
+            max_idx = np.argmax(np.abs(self.map_diff[group[:,0], group[:,1]]))
+            max_cell = group[max_idx]
+
+            # select the most central N examples from this cell
+            cell_samples = data[win_map[tuple(max_cell)]]
+            m = cell_samples.mean(axis=0)
+            min_idx = np.argsort(np.linalg.norm(cell_samples - m, axis=0))[:N]
+            exemplars = cell_samples[min_idx]
+        else:
+            # sample from cells according to their discrepancy
+            p = np.abs(self.map_diff[group[:,0], group[:,1]])
+            p /= p.sum()
+            cell_ids = np.random.choice(len(group), size=N, p=p)
+
+            exemplars = np.empty((N, data.shape[1]))
+            for i, cell_id in enumerate(cell_ids):
+                # select sample randomly from cell
+                cell = group[cell_id]
+                cell_samples = data[win_map[tuple(cell)]]
+                idx = np.random.choice(len(cell_samples), size=1)
+                exemplars[i] = cell_samples[idx]
+
+        if plot:
+            for exemplar in exemplars:
+                plot_data(self._train, self._test, truths=exemplar, **kwargs)
+
+        return exemplars
